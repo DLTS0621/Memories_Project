a1879059637024446c21fd0ce1268cac1e2d6773
DLTS0621 <141294727+DLTS0621@users.noreply.github.com>
2024-09-25 22:46:26 +0530

    Routings

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
 
 dotenv.config(); // Load environment variables
 
 const app = express();
 
 // Middleware
+app.use('/posts', postRoutes);
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
